d23aa78bb0d528a46af506111a7e4a1237910a98
Pranay <pranay@Pranays-MacBook-Pro.local>
2020-04-05 20:07:47 -0400

    Update

--- getVideoStatistics.py
+++ getVideoStatistics.py
@@ -1,12 +1,8 @@
-import json
-
-
-def getStatistics(youtube, vlist, channelName):
-    videoIds = [x["id"]["videoId"] for x in vlist]
+def getStatistics(youtube, videoIds):
     if len(videoIds) <= 50:
         request = youtube.videos().list(part="statistics", maxResults=50, id=",".join(videoIds))
         response = request.execute()
-        writetofile(response["items"], channelName)
+        return response["items"]
     else:
         chunkedVideos = make_chunks(videoIds, 50)
         stats = []
@@ -14,16 +10,9 @@ def getStatistics(youtube, vlist, channelName):
             request = youtube.videos().list(part="statistics", maxResults=50, id=",".join(vids))
             response = request.execute()
             stats.extend(response["items"])
-        writetofile(stats, channelName)
+        return stats
 
 
 def make_chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i : i + n]
-
-
-def writetofile(stats, channelName):
-    fdata = json.dumps(stats)
-    filePtr = open("comments/" + channelName + "_stats.json", "w")
-    filePtr.write(fdata)
-    filePtr.close()
