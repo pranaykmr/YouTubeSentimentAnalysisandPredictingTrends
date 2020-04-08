e8394c590a04b3b07121f2bcdf4c1e84aea6940c
Pranay <pranay@Pranays-MacBook-Pro.local>
2020-04-08 16:27:37 -0400

    JSON Manipulations and data frame construction

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import getVideoIds as fid
 import googleapiclient.discovery
 import google_auth_oauthlib
 import getVideoStatistics as vs
+import pandas as p
 
 with open("constants.json") as json_file:
     constants = json.load(json_file)
@@ -30,10 +31,7 @@ with open("comments/" + channelName + "_vidlist.json") as json_file:
 
 videoIds = [x["id"]["videoId"] for x in vlist]
 stats = vs.getStatistics(youtube, videoIds)
-fdata = json.dumps(stats)
-filePtr = open("comments/" + channelName + "_stats.json", "w")
-filePtr.write(fdata)
-filePtr.close()
+
 
 filePath = "sentimentAnalysis/" + str(channelName) + ".txt"
 sentimentFile = open(filePath, "w")
@@ -46,9 +44,50 @@ for index, v in enumerate(vlist):
     total_comments.extend(comments)
     # sent = syt.sentiment(comments)
     sent = syt.sentimentNew(comments, sentimentFile)
+    positive, negative, neutral = sent
+    for i in range(len(stats)):
+        if stats[i]["id"] == vid:
+            stats[i]["positive"] = (positive / len(comments)) * 100
+            stats[i]["negative"] = (negative / len(comments)) * 100
+            stats[i]["neutral"] = (neutral / len(comments)) * 100
+            stats[i]["title"] = "Video Number : " + str(index + 1) + " --> " + title + "\n"
+            stats[i]["viewCount"] = stats[i]["statistics"]["viewCount"]
+            stats[i]["likeCount"] = stats[i]["statistics"]["likeCount"]
+            stats[i]["dislikeCount"] = stats[i]["statistics"]["dislikeCount"]
+            stats[i]["commentCount"] = stats[i]["statistics"]["commentCount"]
+            del stats[i]["statistics"]
+            break
     print(sent)
     total_sentiment.append(sent)
 
+
+fdata = json.dumps(stats)
+filePtr = open("comments/" + channelName + "_stats.json", "w")
+filePtr.write(fdata)
+filePtr.close()
+
+"""
+Data Frame Created
+
+Data columns (total 11 columns):
+ #   Column        Non-Null Count  Dtype  
+---  ------        --------------  -----  
+ 0   kind          25 non-null     object 
+ 1   etag          25 non-null     object 
+ 2   id            25 non-null     object 
+ 3   positive      25 non-null     float64
+ 4   negative      25 non-null     float64
+ 5   neutral       25 non-null     float64
+ 6   title         25 non-null     object 
+ 7   viewCount     25 non-null     int64  
+ 8   likeCount     25 non-null     int64  
+ 9   dislikeCount  25 non-null     int64  
+ 10  commentCount  25 non-null     int64  
+
+"""
+dataframe = p.read_json("comments/" + channelName + "_stats.json")
+dataframe.info()
+dataframe.shape
 
 sentimentFile.close()
 print("Total Comments Scraped " + str(len(total_comments)))
